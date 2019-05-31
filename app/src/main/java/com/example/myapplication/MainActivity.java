0239ae43ea19b117f061cfa1b9a110244a9bed07
SunilKeshri1998 <sukuke.nitjsr@gmail.com>
2019-05-31 15:50:04 +0530

    just a comment line

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Just for test push in github
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
